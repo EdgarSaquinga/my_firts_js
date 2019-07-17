a57608c0e525222a08a35838d4e9d166b3a9c7b5
yasser <yasser@yasser.example.org>
2019-07-17 18:53:19 -0500

    creando ficheros

---
+++ script.js
@@ -0,0 +1,16 @@
+var numero = 10;
+var decimal = 10.25;
+
+var suma = numero + decimal;
+
+var computadora = {
+    color: "negro",
+    tamaño: "mediano",
+    peso: 200,
+
+}
+
+
+console.log( 'Mi computadora es de color:', computadora.color);
+
+
